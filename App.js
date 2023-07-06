bc55dd4ce5ed39e8d178404a0fd813e62da5dd1f
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-07-05 23:28:53 -0400

    displaying email from jwt_decoded token, checking if token has expired, displaying true or false

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as AppleAuthentication from 'expo-apple-authentication';
+import jwt_decode from "jwt-decode";
 
 export default function App() {
 
     const [appleAuthAvailable, setAppleAuthAvailable] = useState(false);
+    const [userToken, setUserToken] = useState();
 
     useEffect(() => {
         const checkAvailable = async () => {
@@ -24,6 +26,7 @@ export default function App() {
                 ]
             })
             console.log(credential);
+            setUserToken(credential);
             // console logs
             // {
             //     "authorizationCode", 
@@ -47,19 +50,31 @@ export default function App() {
     }
 
     const getAppleAuthContent = () => {
-        return <AppleAuthentication.AppleAuthenticationButton
-            buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-            buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
-            cornerRadius={5}
-            style={styles.button}
-            onPress={loginWithApple}
-        />
+        if (!userToken) {
+            return <AppleAuthentication.AppleAuthenticationButton
+                buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+                buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+                cornerRadius={5}
+                style={styles.button}
+                onPress={loginWithApple}
+            />
+        } else {
+            const decoded = jwt_decode(userToken.identityToken);
+            const current = Date.now() / 1000;
+            console.log(decoded);
+            return (
+                <View>
+                    <Text>{decoded.email}</Text>
+                    <Text>Expired: {(current >= decoded.exp).toString()}</Text>
+                </View>
+            )
+        }
     }
 
     return (
         <View style={styles.container}>
             <Text>Apple Auth Testing!</Text>
-            {appleAuthAvailable 
+            {appleAuthAvailable
                 ? getAppleAuthContent()
                 : <Text>Apple auth unavailable</Text>
             }
@@ -75,7 +90,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    button : {
+    button: {
         width: 200,
         height: 64,
         margin: 10,
