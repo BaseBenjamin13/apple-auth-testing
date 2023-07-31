10bfaca0bdd7edff6af2e7f97d1e240936712e35
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-07-31 00:03:21 -0400

    google login works, added .env

--- components/auth/GoogleAuth.js
+++ components/auth/GoogleAuth.js
@@ -3,26 +3,16 @@ import React, { useState, useEffect } from 'react';
 
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-
-// import {
-//     GoogleSignin,
-//     GoogleSigninButton,
-//     statusCodes,
-// } from '@react-native-google-signin/google-signin';
-
-
-// web client ID: 203758127655-p431i5rar0qp0qs9mq832vf3akoj8a08.apps.googleusercontent.com
-// ios client ID: 203758127655-fitpge8u2dhfjat679schar45g94p6fa.apps.googleusercontent.com
+import { REACT_APP_CLIENT_ID, REACT_APP_IOS_CLIENT_ID } from "@env"
 
 WebBrowser.maybeCompleteAuthSession();
 
 const GoogleAuth = () => {
-
     const [accessToken, setAccessToken] = useState();
     const [user, setUser] = useState();
     const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-        clientId: "203758127655-p431i5rar0qp0qs9mq832vf3akoj8a08.apps.googleusercontent.com",
-        iosClientId: "203758127655-fitpge8u2dhfjat679schar45g94p6fa.apps.googleusercontent.com"
+        clientId: REACT_APP_CLIENT_ID,
+        iosClientId: REACT_APP_IOS_CLIENT_ID
     });
 
     useEffect(() => {
@@ -44,28 +34,6 @@ const GoogleAuth = () => {
         setUser(userInfo)
     }
 
-    // GoogleSignin.configure();
-
-    // const signIn = async () => {
-    //     try {
-    //         await GoogleSignin.hasPlayServices();
-    //         const userInfo = await GoogleSignin.signIn();
-    //           setState({ userInfo });
-    //         console.log(userInfo);
-    //     } catch (error) {
-    //         console.log(error)
-    //         if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-    //             // user cancelled the login flow
-    //         } else if (error.code === statusCodes.IN_PROGRESS) {
-    //             // operation (e.g. sign in) is in progress already
-    //         } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-    //             // play services not available or outdated
-    //         } else {
-    //             // some other error happened
-    //         }
-    //     }
-    // }
-
     // const ShowUserInfo = () => {
     //     if (user) {
     //         return (
@@ -80,7 +48,6 @@ const GoogleAuth = () => {
 
     return (
         <View>
-            {/* <GoogleSigninButton onPress={signIn} /> */}
             <Text style={styles.button}>google component</Text>
             {user
                 ?
